e277f63a7c75f01468164168a770ea5e7ffa4ba5
Olaniyan Eniola <109947938+dot-enny@users.noreply.github.com>
2026-09-05 22:01:44 +0100

    Add: next-greater-element-i - submission-1

---
+++ Data Structures & Algorithms/next-greater-element-i/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        res = [-1] * len(nums1)
+        find = defaultdict()
+        for i in range(len(nums1)): 
+            find[nums1[i]] = i    
+        print(find)
+
+        for i in range(len(nums2)):
+            if nums2[i] in find:
+                for j in range(i+1, len(nums2)):
+                    if nums2[j] > nums2[i]:
+                        res[find[nums2[i]]] = nums2[j]
+                        break
+        return res
+                
