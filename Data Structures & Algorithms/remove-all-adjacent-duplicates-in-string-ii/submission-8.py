d56d46883cfb9348b7de472f72adf07629169e81
Olaniyan Eniola <109947938+dot-enny@users.noreply.github.com>
2026-09-09 22:04:13 +0100

    Add: remove-all-adjacent-duplicates-in-string-ii - submission-8

---
+++ Data Structures & Algorithms/remove-all-adjacent-duplicates-in-string-ii/submission-8.py
@@ -0,0 +1,17 @@
+class Solution:
+    def removeDuplicates(self, s: str, k: int) -> str:
+        # stores [char, current_consecutive_count] pairs
+        stack = []
+        for c in s:
+            if stack:
+                if c != stack[-1][0]:
+                   stack.append([c, 1])
+                else:
+                    stack[-1][1] += 1
+                    if stack[-1][1] == k:
+                        stack.pop()
+            else:
+                stack.append([c, 1])
+        res = [c * s for c, s in stack]
+        
+        return ''.join(res)
