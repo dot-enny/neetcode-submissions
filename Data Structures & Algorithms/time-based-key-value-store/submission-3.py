beb708ec7843466d8509d81e69625e212af59010
Olaniyan Eniola <109947938+dot-enny@users.noreply.github.com>
2026-09-16 23:27:42 +0100

    Add: time-based-key-value-store - submission-3

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-3.py
@@ -0,0 +1,26 @@
+class TimeMap:
+
+    def __init__(self):
+        self.store = defaultdict(list)
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.store[key].append((timestamp, value))
+
+    def get(self, key: str, timestamp: int) -> str:
+        if not len(self.store[key]): return ''
+        
+        l, r = 0, len(self.store[key]) - 1
+        res = (float('inf'), '')
+        
+        while l <= r:
+            m = (l + r) // 2
+            t, v = self.store[key][m]
+            if timestamp == t: res = (t, v) if t > res[0] else res
+            if timestamp >= t: 
+                res = (t, v) if abs(timestamp - t) < abs(timestamp - res[0]) else res
+                l = m + 1
+            else: r = m - 1
+
+        return res[1]
+
+
