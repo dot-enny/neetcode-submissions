d62988d3a9a01b83f16b05fe779b5161a892cd33
Olaniyan Eniola <109947938+dot-enny@users.noreply.github.com>
2026-09-20 23:08:54 +0100

    Add: singlyLinkedList - submission-1

---
+++ Data Structures & Algorithms/singlyLinkedList/submission-1.py
@@ -0,0 +1,56 @@
+class ListNode:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+class LinkedList:
+    
+    def __init__(self):
+        self.head = ListNode(-1)
+        self.tail = self.head
+    
+    def get(self, index: int) -> int:
+        curr = self.head.next
+        i = 0
+        while curr:
+            if i == index: return curr.val
+            curr = curr.next
+            i += 1
+        return -1 # index out of bounds
+
+    def insertHead(self, val: int) -> None:
+        new_node = ListNode(val)
+        new_node.next = self.head.next
+        self.head.next = new_node
+        if not new_node.next:
+            # if list was empty before inserting
+            self.tail = new_node
+
+    def insertTail(self, val: int) -> None:
+        self.tail.next = ListNode(val)
+        self.tail = self.tail.next
+
+    def remove(self, index: int) -> bool:
+        i = 0
+        curr = self.head
+        while i < index and curr:
+            # move curr to the node before the target node
+            curr = curr.next
+            i += 1
+        if curr and curr.next:
+            # first check if node to be removed is tail
+            if self.tail == curr.next:
+                self.tail = curr
+            curr.next = curr.next.next
+            return True
+        return False
+
+    def getValues(self) -> List[int]:
+        res = []
+        curr = self.head.next
+        while curr:
+            res.append(curr.val)
+            curr = curr.next
+        return res
+
+        
